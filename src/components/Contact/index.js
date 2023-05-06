731c5a20b961abeb18e47be980ea76d7f7e175e5
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2023-05-06 18:55:12 +0700

    deploy at github

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,13 +1,20 @@
 import classNames from 'classnames/bind';
-import styles from './Contact.module.scss';
+import styles from './Contact.module.scss'; 
+import Title from '../Common/Title'; 
 const cx = classNames.bind(styles); 
 
 function Contact() {
     return (
-        <div className={cx('contact')} id="contact">
-            <h1>Contact Page</h1>
-        </div>
+      <div className={cx('contact')} id="contact"> 
+        <Title content={"Contact"}></Title>
+        <form>
+          <input placeholder="Name" type="text" id="name" name="name" required/>
+          <input placeholder="Email" type="email" id="email" name="email" required/>
+          <textarea placeholder="Message" id="message" name="message" required></textarea>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     );
-}
-
-export default Contact;
+  }
+  
+  export default Contact;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,75 +5,103 @@ import styles from './Header.module.scss';
 const cx = classNames.bind(styles);
 
 function Header() {
-  const [activeSection, setActiveSection] = useState('');
+    const [activeSection, setActiveSection] = useState('');
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentPosition = window.scrollY;
-      const homeElement = document.getElementById('home');
-      const aboutElement = document.getElementById('about');
-      const skillsElement = document.getElementById('skills');
-      const projectsElement = document.getElementById('projects');
-      const contactElement = document.getElementById('contact');
-
-      if (homeElement.offsetTop <= currentPosition && (homeElement.offsetTop + homeElement.offsetHeight) > currentPosition) {
-        setActiveSection('home');
-      } else if (aboutElement.offsetTop <= currentPosition && (aboutElement.offsetTop + aboutElement.offsetHeight) > currentPosition) {
-        setActiveSection('about');
-      } else if (skillsElement.offsetTop <= currentPosition && (skillsElement.offsetTop + skillsElement.offsetHeight) > currentPosition) {
-        setActiveSection('skills');
-      } else if (projectsElement.offsetTop <= currentPosition && (projectsElement.offsetTop + projectsElement.offsetHeight) > currentPosition) {
-        setActiveSection('projects');
-      } else if (contactElement.offsetTop <= currentPosition && (contactElement.offsetTop + contactElement.offsetHeight) > currentPosition) {
-        setActiveSection('contact');
-      }
+    const handleClick = (event) => {
+        const section = event.target.hash.substr(1);
+        setActiveSection(section);
     };
 
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentPosition = window.scrollY;
+            const homeElement = document.getElementById('home');
+            const aboutElement = document.getElementById('about');
+            const skillsElement = document.getElementById('skills');
+            const projectsElement = document.getElementById('projects');
+            const contactElement = document.getElementById('contact');
 
-  return (
-    <header className={cx('header')}>
-      {/* Logo */}
-      <div className={cx('logo')}>
-        {/* <img /> */}
-        Logo
-      </div>
-      {/* Navigation */}
-      <nav className={cx('nav')}>
-        <ul>
-          <li>
-            <a href="#home" className={activeSection === 'home' ? 'active' : ''}>
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#about" className={activeSection === 'about' ? 'active' : ''}>
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#skills" className={activeSection === 'skills' ? 'active' : ''}>
-              Skills
-            </a>
-          </li>
-          <li>
-            <a href="#projects" className={activeSection === 'projects' ? 'active' : ''}>
-              Projects
-            </a>
-          </li>
-          <li>
-            <a href="#contact" className={activeSection === 'contact' ? 'active' : ''}>
-              Contact
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+            if (
+                homeElement.offsetTop <= currentPosition &&
+                homeElement.offsetTop + homeElement.offsetHeight > currentPosition
+            ) {
+                setActiveSection('home');
+            } else if (
+                aboutElement.offsetTop <= currentPosition &&
+                aboutElement.offsetTop + aboutElement.offsetHeight > currentPosition
+            ) {
+                setActiveSection('about');
+            } else if (
+                skillsElement.offsetTop <= currentPosition &&
+                skillsElement.offsetTop + skillsElement.offsetHeight > currentPosition
+            ) {
+                setActiveSection('skills');
+            } else if (
+                projectsElement.offsetTop <= currentPosition &&
+                projectsElement.offsetTop + projectsElement.offsetHeight > currentPosition
+            ) {
+                setActiveSection('projects');
+            } else if (
+                contactElement.offsetTop <= currentPosition &&
+                contactElement.offsetTop + contactElement.offsetHeight > currentPosition
+            ) {
+                setActiveSection('contact');
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return (
+        <header className={cx('header')}>
+            {/* Logo */}
+            <div className={cx('logo')}>
+                {/* <img /> */}
+                Logo
+            </div>
+            {/* Navigation */}
+            <nav className={cx('nav')}>
+                <ul>
+                    <li>
+                        <a href="#home" onClick={handleClick} className={activeSection === 'home' ? 'active' : ''}>
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#about" onClick={handleClick} className={activeSection === 'about' ? 'active' : ''}>
+                            About
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#skills" onClick={handleClick} className={activeSection === 'skills' ? 'active' : ''}>
+                            Skills
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#projects"
+                            onClick={handleClick}
+                            className={activeSection === 'projects' ? 'active' : ''}
+                        >
+                            Projects
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#contact"
+                            onClick={handleClick}
+                            className={activeSection === 'contact' ? 'active' : ''}
+                        >
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default Header;
