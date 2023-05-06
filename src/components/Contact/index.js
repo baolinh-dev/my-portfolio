1c3697f0580379b95b04a14791ebde9030d59675
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2023-05-07 02:56:22 +0700

    fix contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,20 +1,36 @@
+import Title from '../Common/Title';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import classNames from 'classnames/bind';
-import styles from './Contact.module.scss'; 
-import Title from '../Common/Title'; 
-const cx = classNames.bind(styles); 
+import styles from './Contact.module.scss';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+const cx = classNames.bind(styles);
 
 function Contact() {
     return (
-      <div className={cx('contact')} id="contact"> 
-        <Title content={"Contact"}></Title>
-        <form>
-          <input placeholder="Name" type="text" id="name" name="name" required/>
-          <input placeholder="Email" type="email" id="email" name="email" required/>
-          <textarea placeholder="Message" id="message" name="message" required></textarea>
-          <button type="submit">Submit</button>
-        </form>
-      </div>
+        <div className={cx('contact')} id="contact">
+            <Title content={'Contact'}></Title>
+            <div className={cx('contact-main')}>
+                <div className={cx('contact-infor')}>
+                    <h2>Got a problem to solve?</h2>
+                    <p>Get your space suit ready and tell me your ideas to develop your dream application.</p>
+                    <p className={cx('contact-mail')}>
+                        <span>
+                            {' '}
+                            <FontAwesomeIcon icon={faEnvelope} />{' '}
+                        </span>{' '}
+                        baolinhdev@gmail.com
+                    </p>
+                </div>
+                <form className={cx('contact-form')}>
+                    <input placeholder="Name" type="text" id="name" name="name" required />
+                    <input placeholder="Email" type="email" id="email" name="email" required />
+                    <textarea placeholder="Message" id="message" name="message" required></textarea>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </div>
     );
-  }
-  
-  export default Contact;
+}
+
+export default Contact;
