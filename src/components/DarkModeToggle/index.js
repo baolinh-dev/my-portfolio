b92eb716f3530755dd023682928ea406dcdd53b3
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2023-05-13 05:31:55 +0700

    complete dark mode

--- src/components/DarkModeToggle/index.js
+++ src/components/DarkModeToggle/index.js
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Button from '../Common/Button';
+import Toggle from 'react-toggle';
+import 'react-toggle/style.css';
+import classNames from 'classnames/bind';
+import styles from './DarkModeToggle.module.scss';
+
+const cx = classNames.bind(styles);
 
 function DarkModeToggle({ onClick }) {
     const [isDarkMode, setIsDarkMode] = useState(localStorage.getItem('isDarkMode') === 'true' ? true : false);
@@ -8,17 +14,20 @@ function DarkModeToggle({ onClick }) {
         localStorage.setItem('isDarkMode', isDarkMode);
     }, [isDarkMode]);
 
-    function handleToggleClick() {
+    function handleToggleChange() {
         setIsDarkMode(!isDarkMode);
         onClick(!isDarkMode);
     }
 
     return (
-        <div className="dark-mode-toggle" onClick={handleToggleClick} style={{ position: 'fixed', cursor: 'pointer' }}>
-            <div className={`toggle ${isDarkMode ? 'on' : 'off'}`}>
-                <div className="circle"> ĐÂY </div>
-            </div>
-            <Button onClick={handleToggleClick}>{isDarkMode ? 'Light mode' : 'Dark mode'}</Button>
+        <div className={cx('dark-mode-toggle')}>
+            <Toggle
+                defaultChecked={isDarkMode}
+                icons={false}
+                onChange={handleToggleChange} 
+            /> 
+            <span className={cx('text')}>{isDarkMode ? 'Light mode' : 'Dark mode'}</span>
+            <button onClick={handleToggleChange}>{isDarkMode ? 'Light mode' : 'Dark mode'}</button>
         </div>
     );
 }
