7b5db86cdc7b4c4416388d7abd849d881239b342
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2023-05-31 22:01:10 +0700

    complete split skills array

--- src/components/Constant/index.js
+++ src/components/Constant/index.js
@@ -1,4 +1,4 @@
- const skills = [
+const skills = [
     { name: 'HTML', icon: 'https://img.icons8.com/color/48/null/html-5--v1.png' },
     { name: 'CSS', icon: 'https://img.icons8.com/color/48/null/css3.png' },
     { name: 'SASS', icon: 'https://img.icons8.com/color/48/null/sass.png' },
@@ -10,7 +10,9 @@
     { name: 'MySQL', icon: 'https://img.icons8.com/color/48/null/mysql-logo.png' },
     { name: 'MongoDB', icon: 'https://img.icons8.com/color/48/null/mongodb.png' },
     { name: 'Java', icon: 'https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/181_Java_logo_logos-64.png' },
-];  
+];
 
-export { skills }
- 
+const skillNames = skills.map((skill) => skill.name);
+const skillIcons = skills.map((skill) => skill.icon);  
+
+export { skills, skillNames, skillIcons };
