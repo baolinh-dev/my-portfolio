213b7359a75a1ebe22254726fdf1ead9e0c48ec1
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2023-05-31 16:55:09 +0700

    skills array extraction

---
+++ src/components/Constant/index.js
@@ -0,0 +1,16 @@
+ const skills = [
+    { name: 'HTML', icon: 'https://img.icons8.com/color/48/null/html-5--v1.png' },
+    { name: 'CSS', icon: 'https://img.icons8.com/color/48/null/css3.png' },
+    { name: 'SASS', icon: 'https://img.icons8.com/color/48/null/sass.png' },
+    { name: 'JavaScript', icon: 'https://img.icons8.com/color/48/null/javascript--v1.png' },
+    { name: 'Bootstrap', icon: 'https://img.icons8.com/color/48/null/bootstrap.png' },
+    { name: 'Node.js', icon: 'https://img.icons8.com/color/48/null/nodejs.png' },
+    { name: 'React', icon: 'https://img.icons8.com/plasticine/100/null/react.png' },
+    { name: 'PHP', icon: 'https://img.icons8.com/ios-filled/50/null/php-logo.png' },
+    { name: 'MySQL', icon: 'https://img.icons8.com/color/48/null/mysql-logo.png' },
+    { name: 'MongoDB', icon: 'https://img.icons8.com/color/48/null/mongodb.png' },
+    { name: 'Java', icon: 'https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/181_Java_logo_logos-64.png' },
+];  
+
+export { skills }
+ 
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,21 +1,8 @@
 import Title from '../../components/Common/Title';
 import classNames from 'classnames/bind';
 import styles from './About.module.scss';
+import {skills} from '~/components/Constant' 
 const cx = classNames.bind(styles);
-const skills = [
-    { name: 'HTML', icon: 'https://img.icons8.com/color/48/null/html-5--v1.png' },
-    { name: 'CSS', icon: 'https://img.icons8.com/color/48/null/css3.png' },
-    { name: 'SASS', icon: 'https://img.icons8.com/color/48/null/sass.png' },
-    { name: 'JavaScript', icon: 'https://img.icons8.com/color/48/null/javascript--v1.png' },
-    { name: 'Bootstrap', icon: 'https://img.icons8.com/color/48/null/bootstrap.png' },
-    { name: 'Node.js', icon: 'https://img.icons8.com/color/48/null/nodejs.png' },
-    { name: 'Handlebars', icon: 'https://img.icons8.com/arcade/64/null/handlebar-mustache.png' },
-    { name: 'React', icon: 'https://img.icons8.com/plasticine/100/null/react.png' },
-    { name: 'PHP', icon: 'https://img.icons8.com/ios-filled/50/null/php-logo.png' },
-    { name: 'MySQL', icon: 'https://img.icons8.com/color/48/null/mysql-logo.png' },
-    { name: 'MongoDB', icon: 'https://img.icons8.com/color/48/null/mongodb.png' },
-    { name: 'Java', icon: 'https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/181_Java_logo_logos-64.png' },
-];
 function About() {
     return (
         <div className={cx('about')} id="about">
